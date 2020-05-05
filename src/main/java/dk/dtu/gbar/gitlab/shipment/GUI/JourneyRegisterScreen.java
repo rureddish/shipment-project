c45d82b9dd0fdedddae81cb82d51efe44645f541
krist <krist@KristofersSurface>
2020-05-05 17:08:22 +0200

    Updated Client to allow for new Journeys to be registered to its list. JourneyRegisterScreen now registers new journeys in the loggedIn client

--- src/main/java/dk/dtu/gbar/gitlab/shipment/GUI/JourneyRegisterScreen.java
+++ src/main/java/dk/dtu/gbar/gitlab/shipment/GUI/JourneyRegisterScreen.java
@@ -12,7 +12,11 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import dk.dtu.gbar.gitlab.shipment.Client;
+import dk.dtu.gbar.gitlab.shipment.Journey;
+import dk.dtu.gbar.gitlab.shipment.Location;
 import dk.dtu.gbar.gitlab.shipment.LogIn;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class JourneyRegisterScreen extends JFrame {
 
@@ -22,6 +26,9 @@ public class JourneyRegisterScreen extends JFrame {
 	private LogIn loggedIn;
 	private JButton btnRegister;
 	private JButton btnBack;
+	private JTextField txtOrigin;
+	private JTextField txtDestination;
+	private JTextField txtCargo;
 	
 	
 	public JourneyRegisterScreen(LoginScreen parentWindow,MainMenuScreen mainMenuScreen, LogIn loggedIn) {
@@ -39,6 +46,17 @@ public class JourneyRegisterScreen extends JFrame {
 
 		btnRegister = new JButton("Register");
 		btnRegister.setBounds(10, 260, 150, 29);
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtOrigin.getText().isBlank()||txtDestination.getText().isBlank()||txtCargo.getText().isBlank()) {
+					System.out.println("Please Fill Out All Spaces");
+				}
+				else {
+					loggedIn.getLoggedInClient().registerNewJourney(new Journey(new Location(txtOrigin.getText()),
+							new Location(txtDestination.getText()),loggedIn.getLoggedInClient(),txtCargo.getText()));
+				}
+			}
+		});
 		
 		btnBack = new JButton("Back");
 		btnBack.setLocation(290, 11);
@@ -50,8 +68,38 @@ public class JourneyRegisterScreen extends JFrame {
 			}
 		});
 
+		
+		
+		txtOrigin = new JTextField();
+		txtOrigin.setBounds(180, 105, 96, 20);
+		txtOrigin.setColumns(10);
+		
+		JLabel lblOrigin = new JLabel("Origin:");
+		lblOrigin.setBounds(140, 108, 40, 14);
+		
+		
+		txtDestination = new JTextField();
+		txtDestination.setBounds(180, 136, 96, 20);
+		txtDestination.setColumns(10);
+		
+		JLabel lblDestination = new JLabel("Destination:");
+		lblDestination.setBounds(110, 139, 70, 14);
+		panelJourneyRegistration.add(lblDestination);
+		
+		txtCargo = new JTextField();
+		txtCargo.setBounds(180, 167, 96, 20);
+		txtCargo.setColumns(10);
+		JLabel lblCargo = new JLabel("Cargo:");
+		lblCargo.setBounds(140, 170, 40, 14);
+		
+		
 		panelJourneyRegistration.add(btnBack);
 		panelJourneyRegistration.add(btnRegister);
+		panelJourneyRegistration.add(lblCargo);
+		panelJourneyRegistration.add(txtCargo);
+		panelJourneyRegistration.add(txtDestination);
+		panelJourneyRegistration.add(lblOrigin);
+		panelJourneyRegistration.add(txtOrigin);
 	}
 	
 	private void setEnableButtons(boolean enabled) {
@@ -77,5 +125,4 @@ public class JourneyRegisterScreen extends JFrame {
 		}
 		panelJourneyRegistration.setVisible(visible);
 	}
-
 }
